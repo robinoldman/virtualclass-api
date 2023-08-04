18ee2351e2a53f9eeea85613d2091674f40d4431
robinoldman <robin.c.oldman@gmail.com>
2023-08-04 09:59:21 +0000

    fix bug regarding profile image in posts

--- posts/models.py
+++ posts/models.py
@@ -28,6 +28,7 @@ class Post(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     title = models.CharField(max_length=255)
     content = models.TextField(blank=True)
+   
     image = models.ImageField(
         upload_to='images/', default='../default_post_rgq6aq', blank=True
     )
--- posts/serializers.py
+++ posts/serializers.py
@@ -6,7 +6,7 @@ class PostSerializer(serializers.ModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
-    profile_id = serializers.ReadOnlyField(source='owner.profile.image.url')
+    profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
     
     def validate_image(self, value):
         if value.size > 1024 * 1024 * 2:
