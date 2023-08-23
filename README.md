298c3425af2da331c41161d1aed7d2f66276db21
robinoldman <robin.c.oldman@gmail.com>
2023-08-23 12:48:38 +0000

    add model for lessons which includes owner title content image course and difficulty level

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 Welcome robinoldman,
 
+POSSIBLE DATABASE postgres://mdbzjobfumtyrg:fa41208e2fb417a91d354befaba505c8b456e6581aafe66ad406b1a03bef1941@ec2-52-215-68-14.eu-west-1.compute.amazonaws.com:5432/dc8i184kjh8i7d
+
 This is the Code Institute student template for Gitpod. We have preinstalled all of the tools you need to get started. It's perfectly ok to use this template as the basis for your project submissions.
 
 You can safely delete this README.md file, or change it for your own project. Please do read it at least once, though! It contains some important information about Gitpod and the extensions we use. Some of this information has been updated since the video content was created. The last update to this file was: **September 1, 2021**
--- lessons/models.py
+++ lessons/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Lesson(models.Model):
+    
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    image = models.ImageField(upload_to='lesson_images/', blank=True, null=True)
+    video_url = models.URLField(blank=True, null=True)
+    course = models.ForeignKey(
+        max_length=20, choices=LESSON_CHOICES, default='Maths'
+    )
+    difficulty_level = models.CharField(
+        max_length=20, choices=DIFFICULTY_LEVEL_CHOICES, default='Beginner'
+    )
+   
+   
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title 
