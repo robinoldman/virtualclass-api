9f8a863192edaa2984ff516569e1e2bacfd8516c
robinoldman <robin.c.oldman@gmail.com>
2023-08-28 08:20:00 +0000

    DEBUG = True

--- virtualclas/settings.py
+++ virtualclas/settings.py
@@ -62,7 +62,9 @@ REST_AUTH_SERIALIZERS = {
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEV' in os.environ
+#DEBUG = 'DEV' in os.environ
+
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '8000-robinoldman-virtualclas-003eserfw5m.ws-eu104.gitpod.io',
