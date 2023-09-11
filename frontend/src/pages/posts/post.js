733a208f5bb37356cb94ac147497d94dd7368b08
robinoldman <robin.c.oldman@gmail.com>
2023-09-11 13:39:26 +0000

    add post.js to displaycposts

---
+++ frontend/src/pages/posts/post.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from '../../styles/post.module.css'
+
+const post = (props) => {
+    return ( < div >
+        post stuff < /div>
+    )
+}
+
+export default post
