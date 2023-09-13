fc2150e952e5f9b0b530c6e306cfa194cf8c0984
robinoldman <robin.c.oldman@gmail.com>
2023-09-13 09:44:59 +0000

    created a 3 dot menu for delete and edit funtionality for posts this has not yet been implimented

--- frontend/src/pages/posts/Post.js
+++ frontend/src/pages/posts/Post.js
@@ -4,6 +4,7 @@ import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { Card, Media, OverlayTrigger, Tooltip } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import Avatar from "../../components/Avatar";
+import { MoreDropdown } from "../../components/MoreDropdown";
 
 const Post = (props) => {
   const {
@@ -30,11 +31,13 @@ const Post = (props) => {
         <Media className="align-items-center justify-content-between">
           <Link to={`/profiles/${profile_id}`}>
             <Avatar src={profile_image} height={55} />
+
             {owner}
           </Link>
           <div className="d-flex align-items-center">
             <span>{updated_at}</span>
-            {is_owner && postPage && "..."}
+            <MoreDropdown />
+            {is_owner && postPage}
           </div>
         </Media>
       </Card.Body>
