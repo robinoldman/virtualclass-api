45c44b283e890c8cadd28d1c5f51342aa25df6a8
robinoldman <robin.c.oldman@gmail.com>
2023-09-14 13:42:08 +0000

    add the funcionality to dlete posts - although not quite complete yet

--- frontend/src/pages/posts/PostPage.js
+++ frontend/src/pages/posts/PostPage.js
@@ -8,6 +8,7 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
+import Comment from "../comments/Comment";
 
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
@@ -56,14 +57,17 @@ function PostPage() {
           ) : null}
           {comments.results.length ? (
             comments.results.map((comment) => (
-              <p key={comment.id}>
-                {comment.owner}: {comment.content}
-              </p>
+              <Comment
+                key={comment.id}
+                {...comment}
+                setPost={setPost}
+                setComments={setComments}
+              />
             ))
           ) : currentUser ? (
-            <span> no comments, be the first </span>
+            <span>No comments yet, be the first to comment!</span>
           ) : (
-            <span> no comments yet</span>
+            <span>No comments... yet</span>
           )}
         </Container>
       </Col>
