34d7af0843a0595df97ad43d7a00f4b472599eed
robinoldman <robin.c.oldman@gmail.com>
2023-09-16 12:26:13 +0000

    add image for profile page for avitar

--- frontend/src/contexts/ProfileDataContext.js
+++ frontend/src/contexts/ProfileDataContext.js
@@ -1,6 +1,6 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { axiosReq } from "../api/axiosDefaults";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { useCurrentUser } from "./CurrentUserContext";
 
 const ProfileDataContext = createContext();
 const SetProfileDataContext = createContext();
--- frontend/src/pages/profiles/ProfilePage.js
+++ frontend/src/pages/profiles/ProfilePage.js
@@ -10,29 +10,49 @@ import styles from "../../styles/ProfilePage.module.css";
 import appStyles from "../../App.module.css";
 import btnStyles from "../../styles/Button.module.css";
 
+import PopularProfiles from "./PopularProfiles";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import {
+  useProfileData,
+  useSetProfileData,
+} from "../../contexts/ProfileDataContext";
+import { Button, Image } from "react-bootstrap";
 
 function ProfilePage() {
   const [hasLoaded, setHasLoaded] = useState(false);
   const currentUser = useCurrentUser();
-  const { id } = UseParams();
+  const { id } = useParams();
+  const setProfileData = useSetProfileData;
+  const { pageProfile } = useProfileData();
+  const [profile] = pageProfile.results;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const [{ data: pageProfile }] = await Promise.all([]);
-        axiosReq.get(`/profiles/${id}/`);
+        const [{ data: pageProfile }] = await Promise.all([
+          axiosReq.get(`/profiles/${id}/`),
+        ]);
+        setProfileData((prevState) => ({
+          ...prevState,
+          pageProfile: { results: [pageProfile] },
+        }));
+        setHasLoaded(true);
       } catch (err) {}
     };
-    setHasLoaded(true);
-  }, []);
+    fetchData();
+  }, [id, setProfileData]);
 
   const mainProfile = (
     <>
       <Row noGutters className="px-3 text-center">
         <Col lg={3} className="text-lg-left">
-          <p>Image</p>
+          <Image
+            className={styles.Profileimage}
+            roundedCircle
+            src={profile?.Image}
+          />
         </Col>
         <Col lg={6}>
           <h3 className="m-2">Profile username</h3>
