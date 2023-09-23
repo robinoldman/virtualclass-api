07e093e5d0a05def8c66552b4e82c03b6beb15cd
robinoldman <robin.c.oldman@gmail.com>
2023-09-23 16:21:23 +0000

    doc: add docstrings to explain and describe functionality

--- frontend/src/pages/comments/CommentCreateForm.js
+++ frontend/src/pages/comments/CommentCreateForm.js
@@ -8,25 +8,42 @@ import styles from "../../styles/CommentCreateEditForm.module.css";
 import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
 
+/**
+ * CommentCreateForm component represents a form for creating new comments on a post.*/
+
 function CommentCreateForm(props) {
   const { post, setPost, setComments, profileImage, profile_id } = props;
   const [content, setContent] = useState("");
+
+  /**
+   * Handles changes in the comment content input field.
+   * @param {object} event - The event object representing the input change.
+   */
 
   const handleChange = (event) => {
     setContent(event.target.value);
   };
 
+  /**
+   * Handles the submission of a new comment.
+   * Sends a POST request to create a new comment, updates the comment list, and resets the input field.
+   * @param {object} event - The event object representing the form submission.
+   */
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+      // Send a POST request to create a new comment
       const { data } = await axiosRes.post("/comments/", {
         content,
         post,
       });
+      // Update the list of comments by adding the newly created comment at the beginning
       setComments((prevComments) => ({
         ...prevComments,
         results: [data, ...prevComments.results],
       }));
+      // Update the post data to reflect the increased comments count
       setPost((prevPost) => ({
         results: [
           {
@@ -45,9 +62,11 @@ function CommentCreateForm(props) {
     <Form className="mt-2" onSubmit={handleSubmit}>
       <Form.Group>
         <InputGroup>
+          {/* Link to the user's profile */}
           <Link to={`/profiles/${profile_id}`}>
             <Avatar src={profileImage} />
           </Link>
+          {/* Textarea input for entering the comment content */}
           <Form.Control
             className={styles.Form}
             placeholder="my comment..."
@@ -58,6 +77,7 @@ function CommentCreateForm(props) {
           />
         </InputGroup>
       </Form.Group>
+      {/* Submit button for posting the comment */}
       <button
         className={`${styles.Button} btn d-block ml-auto`}
         disabled={!content.trim()}
