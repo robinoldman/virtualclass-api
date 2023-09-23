bbfd29f43e7cf606fae58030004825669abf3891
robinoldman <robin.c.oldman@gmail.com>
2023-09-23 16:32:32 +0000

    doc: add docstrings to explain and describe functionality

--- frontend/src/pages/comments/CommentEditForm.js
+++ frontend/src/pages/comments/CommentEditForm.js
@@ -5,21 +5,36 @@ import { axiosRes } from "../../api/axiosDefaults";
 
 import styles from "../../styles/CommentCreateEditForm.module.css";
 
+/* Component for editing a comment.*/
 function CommentEditForm(props) {
   const { id, content, setShowEditForm, setComments } = props;
 
+  // State to track the edited content of the comment
   const [formContent, setFormContent] = useState(content);
 
+  /**
+   * Event handler for input change.
+   *
+   * @param {Object} event - The input change event.
+   */
   const handleChange = (event) => {
     setFormContent(event.target.value);
   };
 
+  /**
+   * Event handler for form submission.
+   *
+   * @param {Object} event - The form submission event.
+   */
+
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
+      // Send a PUT request to update the comment
       await axiosRes.put(`/comments/${id}/`, {
         content: formContent.trim(),
       });
+      // Update the comments list with the edited comment content
       setComments((prevComments) => ({
         ...prevComments,
         results: prevComments.results.map((comment) => {
@@ -32,6 +47,7 @@ function CommentEditForm(props) {
             : comment;
         }),
       }));
+      // Hide the edit form
       setShowEditForm(false);
     } catch (err) {
       console.log(err);
@@ -41,6 +57,7 @@ function CommentEditForm(props) {
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="pr-1">
+        {/* Textarea input for editing the comment content */}
         <Form.Control
           className={styles.Form}
           as="textarea"
@@ -50,6 +67,7 @@ function CommentEditForm(props) {
         />
       </Form.Group>
       <div className="text-right">
+        {/* Cancel button to exit edit mode */}
         <button
           className={styles.Button}
           onClick={() => setShowEditForm(false)}
@@ -57,9 +75,10 @@ function CommentEditForm(props) {
         >
           cancel
         </button>
+        {/* Save button to update the comment */}
         <button
           className={styles.Button}
-          disabled={!content.trim()}
+          disabled={!content.trim()} // Disable if the content is empty or whitespace
           type="submit"
         >
           save
