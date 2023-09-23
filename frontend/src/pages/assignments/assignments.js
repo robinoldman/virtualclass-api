5315714924f7041c554784eccac5829486251523
robinoldman <robin.c.oldman@gmail.com>
2023-09-23 11:53:38 +0000

    doc: add docstrings to explain and describe functionality

--- frontend/src/pages/assignments/assignments.js
+++ frontend/src/pages/assignments/assignments.js
@@ -7,6 +7,12 @@ import Avatar from "../../components/Avatar";
 import { axiosRes } from "../../api/axiosDefaults";
 import { MoreDropdown } from "../../components/MoreDropdown";
 
+/**
+ * Represents a component for displaying assignment posts.
+ *
+ * @param {object} props - The assignment post data.
+ */
+
 const Assignments = (props) => {
   const {
     id,
@@ -32,22 +38,34 @@ const Assignments = (props) => {
     setPosts,
   } = props;
 
-  // Add console.log statements to check the values
-
+  // Retrieve the current user from the context
   const currentUser = useCurrentUser();
 
+  // Check if the current user is the owner of this assignment
   const is_owner = currentUser?.username === owner;
+
+  // Get the profile image of the current user
   const profile_image = currentUser?.profile_image;
 
+  // Access the history object for navigation
   const history = useHistory();
 
+  // Log information for debugging purposes
   console.log("profile_image:", profile_image);
   console.log("owner:", owner);
   console.log("image:", image);
 
+  /**
+   * Handles the edit action for the assignment not functional.
+   */
+
   const handleEdit = () => {
     history.push(`/assignments/${id}/edit`);
   };
+
+  /**
+   * Handles the delete action for the assignment not functional.
+   */
 
   const handleDelete = async () => {
     try {
@@ -58,19 +76,22 @@ const Assignments = (props) => {
     }
   };
 
-  // ... rest of your component code
-
   return (
     <Card className={styles.Post}>
       <Card.Body>
         <Media className="align-items-center justify-content-between">
+          {/* Display the avatar of the assignment owner */}
           <Link to={`/profiles/${profile_id}`}>
             <Avatar src={profile_image} height={55} />
+            {/* Display a dropdown for edit and delete actions */}
             <MoreDropdown handleEdit={handleEdit} handleDelete={handleDelete} />
+            {/* Display the owner's username */}
             {owner}
           </Link>
           <div className="d-flex align-items-center">
+            {/* Display the post's last updated date */}
             <span>{updated_at}</span>
+            {/* Display edit and delete dropdown for the owner */}
             {is_owner && lessonsPage && (
               <MoreDropdown
                 handleEdit={handleEdit}
@@ -81,9 +102,11 @@ const Assignments = (props) => {
         </Media>
       </Card.Body>
       <Link to={`/assignments/${id}`}>
+        {/* Display the image of the assignment */}
         <Card.Img src={image} alt={title} />
       </Link>
       <Card.Body>
+        {/* Display the title, content, due date, estimated time, and assigned field */}
         {title && <Card.Title className="text-center">{title}</Card.Title>}
         {content && <Card.Text>{content}</Card.Text>}
         {due_date && <Card.Text>{due_date}</Card.Text>}
